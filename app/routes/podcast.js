5091d3f6635725e124449ae2dafd33fefe989d68
Marcus Poehls <marcus.poehls@gmail.com>
2020-01-07 14:29:08 +0100

    refine title on podcast page

--- app/routes/podcast.js
+++ app/routes/podcast.js
@@ -3,5 +3,9 @@
 module.exports = {
   method: 'GET',
   path: '/podcast',
-  handler: (_, h) => h.view('podcast', null, { layout: 'startpage' })
+  handler: (_, h) => h.view('podcast', {
+    title: 'A Supercharge Tale Podcast'
+  }, {
+    layout: 'startpage'
+  })
 }
