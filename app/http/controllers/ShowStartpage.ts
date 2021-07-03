7a09b116a82da76945ff1dd0de4fee9b58c46aff
Marcus Pöhls <marcus.poehls@gmail.com>
2021-07-03 17:03:48 +0200

    use layout

--- app/http/controllers/ShowStartpage.ts
+++ app/http/controllers/ShowStartpage.ts
@@ -7,6 +7,8 @@ export class ShowStartpage {
    * Handle the given request.
    */
   handle ({ response }: HttpContext): unknown {
-    return response.view('index')
+    return response.view('index', builder => {
+      builder.layout('startpage')
+    })
   }
 }
