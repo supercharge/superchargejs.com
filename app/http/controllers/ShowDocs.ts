401a9ec3ac1beaeca40ed6fee0b0c4c61a2fda1c
Marcus Pöhls <mp@seda.digital>
2021-07-09 16:17:35 +0200

    HttpRedirect

--- app/http/controllers/ShowDocs.ts
+++ app/http/controllers/ShowDocs.ts
@@ -1,13 +1,13 @@
 'use strict'
 
 import { Config } from '@supercharge/facades'
-import { HttpContext } from '@supercharge/contracts'
+import { HttpContext, HttpRedirect } from '@supercharge/contracts'
 
 export class ShowDocs {
   /**
    * Handle the given request.
    */
-  handle ({ response }: HttpContext): unknown {
+  handle ({ response }: HttpContext): HttpRedirect {
     const version = Config.get('docs.default', 'main') as string
 
     return response.redirect().to(`/docs/${version}`)
