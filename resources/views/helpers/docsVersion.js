e44818ce730e9a6c10a28edf5d33b567094df9c6
Marcus Pöhls <mp@seda.digital>
2021-11-11 12:12:26 +0100

    update default

--- resources/views/helpers/docsVersion.js
+++ resources/views/helpers/docsVersion.js
@@ -1,10 +1,10 @@
 'use strict'
 
 /**
- * Returns the given `version` or falls back to `master`.
+ * Returns the given `version` or falls back to the configured default.
  *
  * @returns {String}
  */
 module.exports = function docsVersion (version) {
-  return version || 'master'
+  return version || '2.x'
 }
