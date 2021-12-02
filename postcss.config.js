3067a57ec2322a2ad1bb0e6c7eda2d44f55fbe47
Marcus Pöhls <mp@seda.digital>
2021-12-02 11:43:30 +0100

    minify CSS in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
 'use strict'
 
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
-  ]
+const cssnano = require('cssnano')
+
+module.exports = ({ env }) => {
+  return {
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ].concat(
+      env === 'production'
+        ? [cssnano({ preset: 'default' })]
+        : []
+    )
+  }
 }
