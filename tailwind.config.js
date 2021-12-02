384939074cc697d9f9e27c25f1a5e4d3643bf057
Marcus Pöhls <mp@seda.digital>
2021-12-02 09:47:20 +0100

    refine prose styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,48 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        sm: '.825rem',
+        md: '.875rem'
+      },
       fontFamily: {
         sans: ['ATC Overlook', 'BRHendrix', ...defaultTheme.fontFamily.sans],
         brhendrix: ['BRHendrix', ...defaultTheme.fontFamily.sans],
+        mono: ['Cascadida Code', ...defaultTheme.fontFamily.mono],
+      },
+      typography (theme) {
+        return {
+          DEFAULT: {
+            css: {
+              color: theme('colors.slate.600'),
+
+              h1: {
+                color: theme('colors.gray.900'),
+              },
+
+              h2: {
+                color: theme('colors.gray.900'),
+                'margin-bottom': theme('margin.3'),
+                'margin-top': theme('margin.16')
+              },
+
+              h3: {
+                color: theme('colors.gray.900'),
+                'margin-bottom': theme('margin.2'),
+                'margin-top': theme('margin.14')
+              },
+
+              h4: {
+                color: theme('colors.gray.900'),
+                'margin-top': theme('margin.12')
+              },
+
+              pre: {
+
+              },
+            },
+          }
+        }
       },
     }
   },
