f9d0ce3724ec423cad5716a77153233e78fb0ff1
Marcus Pöhls <marcus.poehls@gmail.com>
2021-12-05 14:00:04 +0100

    override

--- app/console/commands/inspire.ts
+++ app/console/commands/inspire.ts
@@ -6,7 +6,7 @@ export default class Inspire extends Command {
   /**
    * Configure this command.
    */
-  configure (): void {
+  override configure (): void {
     this
       .name('inspire')
       .description('Print an inspiring quote.')
@@ -15,7 +15,7 @@ export default class Inspire extends Command {
   /**
    * Run the command action.
    */
-  async run (): Promise<any> {
+  override async run (): Promise<any> {
     console.log('Sometimes you win. Sometimes you learn.')
   }
 }
