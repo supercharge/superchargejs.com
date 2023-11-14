7b7e7dc639a109731b9aadf487807c5325ff66e1
Marcus Pöhls <mp@seda.digital>
2023-11-14 09:38:46 +0100

    add types

---
+++ types/http.ts
@@ -0,0 +1,12 @@
+
+declare module '@supercharge/contracts' {
+  export interface HttpRequestHeaders {
+    // add custom HTTP request headers, like
+    // 'your-header-name': string | undefined
+  }
+
+  export interface HttpStateData {
+    // add custom HTTP request headers, like
+    // 'user': User | undefined
+  }
+}
